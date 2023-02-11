134d146d293c408614d1852ba5bbf0e0e33e80cf
siida <physicshinzui@gmail.com>
2023-02-12 08:54:22 +0900

    Added get_interacting_residues.py

--- get_interacting_residues.py
+++ get_interacting_residues.py
@@ -1,26 +1,35 @@
+#!/usr/bin/env python 
 from pymol import cmd 
-import sys
+import argparse
 
 print("""
 Description:
-    This script is used to print interacting residues between two molecules in two PDB files.
+    This script is used to view interacting residues of a receptor with another.
     
 Usage: 
-    pymol -c get_interacting_residues.py -- protein.pdb ligand.pdb 
+    pymol -c get_interacting_residues.py -- receptor.pdb binder.pdb 
 """)
+p = argparse.ArgumentParser()
+p.add_argument("-r", "--receptor", type=str, required=True)
+p.add_argument("-b", "--binder", type=str, required=True)
+p.add_argument("-v", "--view", type=bool, required=False, default=False)
+args = p.parse_args()
+receptor = args.receptor
+binder = args.binder
 
-cmd.load(sys.argv[1], "protein")
-cmd.load(sys.argv[2], "ligand")
-cmd.do("sele polymer and name ca within 6.0 of ligand")
-cmd.show("spheres", "sele")
+cmd.load(receptor, "receptor")
+cmd.load(binder, "binder")
+cmd.do("sele interface, receptor and name ca within 6.0 of binder")
+cmd.show("spheres", "interface")
+cmd.do("show sticks, byres interface")
+#cmd.do("com sele")
 residues = []
-cmd.iterate("sele", "residues.append((resi, resn))")
 
 # Print the residues
 ri = []
-for residue in residues:
-    print("Residue:", residue[0], "Residue Name:", residue[1])
-    ri.append(residue[0])
+for atom in cmd.get_model("interface").atom:
+    print("Residue:", atom.resn+atom.resi, "Atom Name:", atom.name)
+    ri.append(atom.resi)
 print("---Gromacs selection---")
 print("Copy and paste the following command in the Gromacs shell (gmx make_ndx):") 
 print("ri", *ri)
